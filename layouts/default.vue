728d73887f5c747a6251129564f3a550b2f6abb0
HamidNE <hamidne@mail.ru>
2019-11-13 09:09:25 +0330

    add to git

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<nuxt />
+		<custom-footer />
+	</div>
+</template>
+
+<script>
+import CustomFooter from "~/components/layouts/CustomFooter";
+
+export default {
+	components: { CustomFooter }
+};
+</script>
+
+<style src="shabnam-font/dist/font-face.css"></style>
---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+import fire from "./config/fire";
+import head from "./config/head";
+import toast from "./config/toast";
+import modules from "./config/modules";
+import sitemap from "./config/sitemap";
+import plugins from "./config/plugins";
+import generate from "./config/generate";
+import manifest from './config/manifest';
+import bootstrapVue from "./config/bootstrapVue";
+import pageTransition from "./config/pageTransition";
+
+export default {
+	mode: "universal",
+
+  head,
+  fire,
+  toast,
+	plugins,
+  modules,
+  sitemap,
+  generate,
+  manifest,
+  bootstrapVue,
+  pageTransition,
+
+	loading: { color: "#34bfa3" },
+
+	css: [{ src: "@/assets/scss/app.scss", lang: "scss" }],
+
+	build: {
+		extend(config, ctx) {}
+	}
+};
