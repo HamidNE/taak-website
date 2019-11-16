3ffdc6ad3d09806bc80bb62125710deaf305d533
HamidNE <hamidne@mail.ru>
2019-11-16 10:01:14 +0330

    fixed seo tags bug

--- utils/seo/webinar.js
+++ utils/seo/webinar.js
@@ -4,31 +4,45 @@
  * @return {{meta: *[], link: {rel: string, href: *}[], title: *}}
  */
 export default webinar => {
-  const url = `https://taakproject.ir/webinars/${webinar.slug}`;
-  const image = `https://taakproject.ir/img/webinars/${webinar.slug}.jpg`;
+	const url = `https://taakproject.ir/webinars/${webinar.slug}`;
+	const image = `https://taakproject.ir/img/webinars/${webinar.slug}.jpg`;
 
-  return {
-    title: webinar.title,
-    meta: [
-      {name: 'description', content: webinar.description},
+	return {
+		title: webinar.title,
+		meta: [
+			{ name: "description", content: webinar.description },
 
-      // OpenGraph
-      {property: 'og:url', content: url},
-      {property: 'og:type', content: 'video:webinar'},
-      {property: 'og:title', content: webinar.title},
-      {property: 'og:image', content: image},
-      {property: 'og:video', content: webinar.links[0].value},
-      {property: 'og:site_name', content: 'Taak Project'},
-      {property: 'og:description', content: webinar.description},
+			// OpenGraph
+			{ hid: "og:url", property: "og:url", content: url },
+			{ hid: "og:type", property: "og:type", content: "video:webinar" },
+			{ hid: "og:title", property: "og:title", content: webinar.title },
+			{ hid: "og:image", property: "og:image", content: image },
+			{
+				hid: "og:video",
+				property: "og:video",
+				content: webinar.links[0].value
+			},
+			{
+				hid: "og:site_name",
+				property: "og:site_name",
+				content: "Taak Project"
+			},
+			{
+				hid: "og:description",
+				property: "og:description",
+				content: webinar.description
+			},
 
-      // Twitter
-      {name: 'twitter:card', content: 'webinar'},
-      {name: 'twitter:title', content: webinar.title},
-      {name: 'twitter:image', content: image},
-      {name: 'twitter:description', content: webinar.description},
-    ],
-    'link': [
-      {rel: 'canonical', href: url}
-    ]
-  };
+			// Twitter
+			{ hid: "twitter:card", name: "twitter:card", content: "webinar" },
+			{ hid: "twitter:title", name: "twitter:title", content: webinar.title },
+			{ hid: "twitter:image", name: "twitter:image", content: image },
+			{
+				hid: "twitter:description",
+				name: "twitter:description",
+				content: webinar.description
+			}
+		],
+		link: [{ rel: "canonical", href: url }]
+	};
 };
